41bdce2d111fe2ae4e0c5fa9401823fff8c3d24b
Mike McNeil <mike@balderdash.co>
2015-02-15 17:18:32 -0800

    Finishes decorating the metadata of choose-from-list.

--- machines/choose-from-list.js
+++ machines/choose-from-list.js
@@ -13,11 +13,12 @@ module.exports = {
   inputs: {
 
     choices: {
-      description: 'The list of choices: the `name` property indicates how the choice will appear in the list.',
+      description: 'The list of choices- where each choice is a name/value pair (dictionary).',
+      extendedDescription: 'The `name` property indicates how the choice will appear in the list.  The `value` property is used to report which choice was selected, so each `value` should be unique within the list of choices.',
       example: [
         {
           name: 'Choice A',
-          value: 'choice-a'
+          value: 'some-unique-identifier'
         }
       ],
       required: true
@@ -48,7 +49,8 @@ module.exports = {
     },
 
     success: {
-      description: 'Done.'
+      description: 'Returned the `value` property of the choice that was selected.',
+      example: 'some-unique-identifier'
     }
 
   },
